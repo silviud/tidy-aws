232fa9e17d7cd0efe119d7d0e5bd4d1fea747998
Silviu Dicu <silviu.dicu@ubisoft.com>
2026-02-11 16:15:55 -0500

    csv out

--- tidy_aws/list-instances.py
+++ tidy_aws/list-instances.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 
 import boto3
@@ -11,15 +12,37 @@ def list_running_instances():
     # Create an EC2 resource
     ec2 = boto3.resource('ec2', region_name=AWS_REGION)
 
-    # Filter to get only running instances
     filters = [{'Name': 'instance-state-name', 'Values': ['running']}]
-
-    # Retrieve instances
     instances = ec2.instances.filter(Filters=filters)
 
-    # Print instance details
-    for instance in instances:
-        print(f'Instance ID: {instance.id}, State: {instance.state["Name"]}, Type: {instance.instance_type}, Launch Time: {instance.launch_time}')
+    return instances
+
+
+def print_instances(instances: list, f_type: str = 'table'):
+
+    if f_type == 'default':
+        for instance in instances:
+            print(f'Instance ID: {instance.id}, State: {instance.state["Name"]}, Type: {instance.instance_type}, Launch Time: {instance.launch_time}')
+    elif f_type == 'csv':
+        if instances:
+            print('InstanceID, State, Type, LaunchTime')
+            for instance in instances:
+                print(f'{instance.id}, {instance.state["Name"]}, {instance.instance_type}, {instance.launch_time}')
+    else:
+        print(f'Unsupported format {f_type}')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--format', action='store', default='default', choices=('csv', 'default'))
+
+    args = parser.parse_args()
+
+    format_type = args.format
+
+    instances = list_running_instances()
+    print_instances(instances, format_type)
+
 
 if __name__ == "__main__":
-    list_running_instances()
+    main()
